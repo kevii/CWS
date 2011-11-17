b361bc1463ee1457a92aefcb57390a7e31b7dadb
KEVIN CHENG <kevin@KEVIN-T410.(none)>
2011-11-17 18:26:04 +0900

    ngram av calculation

--- DataProcess.rb
+++ DataProcess.rb
@@ -13,18 +13,22 @@ class DataProcess
           temp = ""
         elsif
           temp += line[0]
+          puts temp.length
           puts "uni, bi: #{temp[2]}, #{temp[1..2]}, #{temp[0..2]}"
-	  ((l - 1)..0).each do |i|
-	    
-#          if(temp.length == l)
-            if(av[temp[i..(l - 1)]] == nil)
-              av[temp[i..(l - 1)]] = af.process(temp)
-            elsif
-              puts "Existed pattern:#{temp}: av#{av[temp[i..(l - 1)]]}"
-              i += 1
+          (1..l).each do |i|
+            if(temp.length == i)
+              (0..temp.length-1).each do |j|
+                if(av[temp[j..temp.length - 1]] == nil)
+                  # puts i
+                  av[temp[j..temp.length - 1]] = af.process(temp[j..temp.length - 1])
+                elsif
+                  puts "Existed pattern:#{temp[j..temp.length - 1]}: av#{av[temp[j..temp.length - 1]]}"
+                end
+              end
+              if(i == l)
+                temp = temp[1..i-1]
+              end
             end
-#	    puts temp
-            temp = temp[1..(l-1)]
           end
         end
         # puts "line: #{index}"
